409cdd7bf3d10800376206bb2fbb9c1b29fec752
AnilGupta001 <ga6119480@gmail.com>
2023-08-10 17:48:25 +0530

    Morn

---
+++ Mern Front-End/db/conn.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', false);
+
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(() => console.log("DataBase Connected")).catch((err) => {
+    console.log(err);
+});
